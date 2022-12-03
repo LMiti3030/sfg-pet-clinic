49fe4c05dc0d5c9d25b14e77652cfeb9762ab207
LMiti3030 <mititelu_laura@yahoo.com>
2022-12-03 10:48:49 +0200

    Adding index page and controller, closes #8

---
+++ pet-clinic-web/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package mititelu.laura.guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author LMiti3030
+ * created on 03.12.2022
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index"; //view name
+    }
+}
