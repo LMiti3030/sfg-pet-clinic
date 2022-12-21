900014291b61842e840c177237841e900f0beb53
LMiti3030 <mititelu_laura@yahoo.com>
2022-12-21 15:52:29 +0200

    added vets list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package mititelu.laura.guru.springframework.sfgpetclinic.controllers;
 
+import mititelu.laura.guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model)
+    {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
