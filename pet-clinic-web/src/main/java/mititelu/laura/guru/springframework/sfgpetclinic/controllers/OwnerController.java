bc5fce8eda90125270517eff23eec9d5f7975de6
LMiti3030 <mititelu_laura@yahoo.com>
2022-12-21 15:41:54 +0200

    added owners list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package mititelu.laura.guru.springframework.sfgpetclinic.controllers;
 
+import mititelu.laura.guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,8 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 
