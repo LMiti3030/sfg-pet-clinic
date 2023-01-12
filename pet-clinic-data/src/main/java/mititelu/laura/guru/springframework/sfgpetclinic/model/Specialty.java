9309f21ac2b3ea1a0d91c500f7aff2b17c8137a1
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-12 17:55:38 +0200

    added Specialty, updated vet. closes #27

---
+++ pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package mititelu.laura.guru.springframework.sfgpetclinic.model;
+
+public class Specialty  extends BaseEntity{
+
+    private String descripton;
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
+}
--- pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,11 +1,20 @@
 package mititelu.laura.guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author LMiti3030
  * created on 02.12.2022
  */
 public class Vet extends Person{
 
+    private Set<Specialty> specialties;
 
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
 
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
