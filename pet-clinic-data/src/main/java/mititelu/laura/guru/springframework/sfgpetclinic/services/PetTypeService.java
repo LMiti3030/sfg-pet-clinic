dac79ef9c28069afe621ad890073b227ba64c8d9
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-12 18:10:34 +0200

    added PetTypeMapService. closes #29

---
+++ pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package mititelu.laura.guru.springframework.sfgpetclinic.services;
+
+import mititelu.laura.guru.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
---
+++ pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package mititelu.laura.guru.springframework.sfgpetclinic.services.map;
+
+import mititelu.laura.guru.springframework.sfgpetclinic.model.PetType;
+import mititelu.laura.guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findByID(id);
+    }
+}
