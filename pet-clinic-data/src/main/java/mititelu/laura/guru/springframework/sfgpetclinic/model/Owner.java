c7112912f7a7637369d0936d8017a0f81ecd7715
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-12 17:51:57 +0200

    added Visit entity, updated owner with pet relations. closes #26

--- pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package mititelu.laura.guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author LMiti3030
  * created on 02.12.2022
  */
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
