369d21b3e959b6a41895707eebd348646a361f7d
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-12 18:16:04 +0200

    Added PetTypes to Bootstrap DataLoader. closes #32

--- pet-clinic-web/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package mititelu.laura.guru.springframework.sfgpetclinic.bootstrap;
 
 import mititelu.laura.guru.springframework.sfgpetclinic.model.Owner;
+import mititelu.laura.guru.springframework.sfgpetclinic.model.PetType;
 import mititelu.laura.guru.springframework.sfgpetclinic.model.Vet;
 import mititelu.laura.guru.springframework.sfgpetclinic.services.OwnerService;
+import mititelu.laura.guru.springframework.sfgpetclinic.services.PetTypeService;
 import mititelu.laura.guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -17,14 +19,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService,VetService vetService ){
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService){
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Laura");
         owner1.setLastName("Weston");
