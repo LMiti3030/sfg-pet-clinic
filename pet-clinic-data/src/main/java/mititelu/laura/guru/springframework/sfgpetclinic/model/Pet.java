87c710fbceec806bee79ddee346e797aa62b3180
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-23 17:33:34 +0200

    added jpa mapping for visit + modified pet Closes #38

--- pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package mititelu.laura.guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author LMiti3030
@@ -24,6 +26,9 @@ public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -55,5 +60,13 @@ public class Pet extends BaseEntity{
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
