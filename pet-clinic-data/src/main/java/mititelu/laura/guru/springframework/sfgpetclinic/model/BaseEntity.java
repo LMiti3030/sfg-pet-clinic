dd2ec9ad741dd0699ee6137fd9456b2d5a41d6a7
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-23 16:54:18 +0200

    added base entity jpa. Closes #35

--- pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,13 +1,20 @@
 package mititelu.laura.guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * @author LMiti3030
  * created on 02.12.2022
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
