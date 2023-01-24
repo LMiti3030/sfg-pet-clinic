df646544d98afb944d41586ccb1fa135bee8fadc
LMiti3030 <mititelu.laura@yahoo.com>
2023-01-24 09:46:19 +0200

    added pet spring data jpa service Closes #43

---
+++ pet-clinic-data/src/main/java/mititelu/laura/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package mititelu.laura.guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import mititelu.laura.guru.springframework.sfgpetclinic.model.Pet;
+import mititelu.laura.guru.springframework.sfgpetclinic.repositories.PetRepository;
+import mititelu.laura.guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return null;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
